c98ecbe933e58493bd7389274aeacbcf100e927d
LinXin <skv@live.com>
2026-04-08 16:29:52 +0800

    update

--- models/proxy.go
+++ models/proxy.go
@@ -1,12 +1,12 @@
 package models
 
 type Proxy struct {
-	Name      string `json:"name" toml:"name"`
-	Type      string `json:"type" toml:"type"`
-	LocalIP   string `json:"localIP" toml:"localIP"`
-	LocalPort int    `json:"localPort" toml:"localPort"`
-	RemotePort int   `json:"remotePort" toml:"remotePort"`
-	Comment   string `json:"comment" toml:"-"` // comment不写入toml，存储在单独文件
+	Name       string `json:"name" toml:"name"`
+	Type       string `json:"type" toml:"type"`
+	LocalIP    string `json:"localIP" toml:"localIP"`
+	LocalPort  int    `json:"localPort" toml:"localPort"`
+	RemotePort int    `json:"remotePort" toml:"remotePort"`
+	Comment    string `json:"comment" toml:"-"` // comment不写入toml，存储在单独文件
 }
 
 // ProxyComments 存储代理备注的映射
@@ -14,11 +14,11 @@ type ProxyComments map[string]string
 
 // ProxyForToml 用于序列化到toml的代理结构（不包含comment）
 type ProxyForToml struct {
-	Name      string `toml:"name"`
-	Type      string `toml:"type"`
-	LocalIP   string `toml:"localIP"`
-	LocalPort int    `toml:"localPort"`
-	RemotePort int   `toml:"remotePort"`
+	Name       string `toml:"name"`
+	Type       string `toml:"type"`
+	LocalIP    string `toml:"localIP"`
+	LocalPort  int    `toml:"localPort"`
+	RemotePort int    `toml:"remotePort"`
 }
 
 type AuthConfig struct {
@@ -56,4 +56,3 @@ type UpdateProxyRequest struct {
 type DeleteProxyRequest struct {
 	Index int `json:"index" binding:"required"`
 }
-
